069afe8bbf3b8d628e4408d8ebec7946f8527b93
Rahul Kishore <96075939+Rahulkishore1995@users.noreply.github.com>
2022-03-31 15:32:10 +0530

    Add files via upload
    ham banye hai

---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+app.listen(5000, async () => {
+    try{
+        await connect();
+        console.log("listening on port 5000")
+    }
+    catch(err){
+        console.log(err.message);
+    }
+});
